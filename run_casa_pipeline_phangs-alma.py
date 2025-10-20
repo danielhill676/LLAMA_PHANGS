f444ee694850f09558a286f2e381b71e8c682cc5
danielhill676 <daniel.hill6767@gmail.com>
2025-10-20 19:09:32 +0100

    added missing files, CHANGED CELL AND IMAGE OVERRIDE IN HANDLERIMAGING, set up runcasepipeline for 2775 combined

--- run_casa_pipeline_phangs-alma.py
+++ run_casa_pipeline_phangs-alma.py
@@ -129,11 +129,11 @@ this_kh.make_missing_directories(imaging=True, derived=True, postprocess=True, r
 #  "NGC5921", "NGC6814" ,"NGC7172", "NGC718",  "NGC7213" ])
 
 this_uvh.set_targets(only=[
-'NGC3351','NGC4254',])
+'NGC2775'])
 
 
 #this_uvh.set_interf_configs(only=['12m', '7m','12m+7m'])
-this_uvh.set_interf_configs(only=['12m', '7m','12m+7m'])
+this_uvh.set_interf_configs(only=['12m'])
 this_uvh.set_line_products(only=['co21'])
 this_uvh.set_no_cont_products(False)
 
@@ -144,24 +144,23 @@ this_uvh.set_no_cont_products(False)
 
 
 this_imh.set_targets(only=[
-'NGC3351','NGC4254',])
-
-this_imh.set_interf_configs(only=['12m', '7m','12m+7m'])
+'NGC2775'])
+this_imh.set_interf_configs(only=['7m','12m+7m'])
 this_imh.set_no_cont_products(False)
 this_imh.set_line_products(only=['co21'])
 
 
 
 this_pph.set_targets(only=[
-'NGC3351','NGC4254',])
+'NGC2775'])
 
-this_pph.set_interf_configs(only=['12m', '7m','12m+7m'])
-this_pph.set_feather_configs(only=['12m', '7m','12m+7m'])
+this_pph.set_interf_configs(only=['7m','12m+7m'])
+this_pph.set_feather_configs(only=['7m','12m+7m'])
 
 # Use boolean flags to set the steps to be performed when the pipeline
 # is called. See descriptions below (but only edit here).
 
-do_staging = False
+do_staging = True
 do_imaging = True
 do_postprocess = True
 do_stats = True
