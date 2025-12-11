739e9e7c90142922e67b801bdca47ac09df674ce
danielhill676 <daniel.hill6767@gmail.com>
2025-12-11 13:08:44 +0000

    maybe this will fix why the derived is not running in full for some targets?

--- run_casa_pipeline_phangs-alma.py
+++ run_casa_pipeline_phangs-alma.py
@@ -129,11 +129,11 @@ this_kh.make_missing_directories(imaging=True, derived=True, postprocess=True, r
 #  "NGC5921", "NGC6814" ,"NGC7172", "NGC718",  "NGC7213" ])
 
 this_uvh.set_targets(only=[
-'NGC3081','NGC5845','NGC2110'])
+'NGC3351','NGC4254'])
 
 
 #this_uvh.set_interf_configs(only=['12m', '7m','12m+7m'])
-this_uvh.set_interf_configs(only=['12m+7m'])
+this_uvh.set_interf_configs(only=['12m'])
 this_uvh.set_line_products(only=['co21'])
 this_uvh.set_no_cont_products(False)
 
@@ -144,26 +144,25 @@ this_uvh.set_no_cont_products(False)
 
 
 this_imh.set_targets(only=[
-'NGC3081','NGC5845','NGC2110'])
-
-this_imh.set_interf_configs(only=['12m+7m'])
+'NGC3351','NGC4254'])
+this_imh.set_interf_configs(only=['12m'])
 this_imh.set_no_cont_products(False)
 this_imh.set_line_products(only=['co21'])
 
 
 
 this_pph.set_targets(only=[
-'NGC3081','NGC5845','NGC2110'])
+'NGC3351','NGC4254'])
 
-this_pph.set_interf_configs(only=['12m+7m'])
+this_pph.set_interf_configs(only=['12m'])
 this_pph.set_no_cont_products(False)
-this_pph.set_feather_configs(only=['12m+7m'])
+this_pph.set_feather_configs(only=['12m'])
 
 # Use boolean flags to set the steps to be performed when the pipeline
 # is called. See descriptions below (but only edit here).
 
-do_staging = False
-do_imaging = False
+do_staging = True
+do_imaging = True
 do_postprocess = True
 do_stats = True
 
